7026524bbb4a88e4d5fbdb0902bbe0c92f408c58
priti <pritibhushanf@gmail.com>
2026-05-07 22:47:33 +0530

    rentcalProject

--- rentCal.py
+++ rentCal.py
@@ -4,7 +4,7 @@ totsal rent
 total food ordered
 electricity units spend
 charge per unit
-
+persons in flat
 Output
 total amt have to pay
 '''
@@ -13,3 +13,10 @@ rent = int(input ('Enter flat rent :'))
 food = int(input('Enter amout of food ordered = '))
 electricity_spend = int(input('enter eletricity total = '))
 charge_per_unit = int(input('Charge per unit ?? '))
+persons = int(input('enter number of persons = '))
+
+totl_e_bill = charge_per_unit * electricity_spend
+
+output =(food + rent +totl_e_bill)// persons
+
+print("rent need to pay each = ",output)
