44f53f9b5072caf0ae6292b8f0fb44f0a1df3d97
priti <pritibhushanf@gmail.com>
2026-05-07 22:44:05 +0530

    rentcalProject

---
+++ rentCal.py
@@ -0,0 +1,15 @@
+'''
+Input is
+totsal rent
+total food ordered
+electricity units spend
+charge per unit
+
+Output
+total amt have to pay
+'''
+
+rent = int(input ('Enter flat rent :'))
+food = int(input('Enter amout of food ordered = '))
+electricity_spend = int(input('enter eletricity total = '))
+charge_per_unit = int(input('Charge per unit ?? '))
